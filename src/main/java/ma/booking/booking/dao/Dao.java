ee6d31ba0d5b9fc981aa960f1f3782ea9c108c56
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-10-13 16:41:23 +0100

    add DAO interface

---
+++ src/main/java/ma/booking/booking/dao/Dao.java
@@ -0,0 +1,16 @@
+package ma.booking.booking.dao;
+
+import java.util.ArrayList;
+
+public interface Dao<T> {
+
+     T getById(long id);
+
+    ArrayList<T> getAll();
+
+    void saveItem(T t);
+
+    void updateItem(T t, String[] params);
+
+    void delete(T t);
+}
